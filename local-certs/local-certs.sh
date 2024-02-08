d010fee471326d2225e152ab6bc2f79502e018e7
Zoe Hay <zoemhay@gmail.com>
2024-02-08 12:51:16 -0800

    add local angular-e-commerce and certs

--- local-certs/local-certs.sh
+++ local-certs/local-certs.sh
@@ -8,3 +8,7 @@ openssl req -x509 -out ./certs/localhost.crt -keyout ./certs/localhost.key \
 openssl req -x509 -out ./certs/e-commerce.localhost.crt -keyout ./certs/e-commerce.localhost.key \
     -newkey rsa:2048 -nodes -sha256 \
     -subj '/CN=e-commerce.localhost' -extensions EXT -config <(printf "[dn]\nCN=e-commerce.localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:e-commerce.localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+openssl req -x509 -out ./certs/angular-e-commerce.localhost.crt -keyout ./certs/angular-e-commerce.localhost.key \
+    -newkey rsa:2048 -nodes -sha256 \
+    -subj '/CN=angular-e-commerce.localhost' -extensions EXT -config <(printf "[dn]\nCN=angular-e-commerce.localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:angular-e-commerce.localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
